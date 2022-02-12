ef6f36f9ae1c982392215b70716d8975ddfd7c5c
Cleiton <cleitonrmacedo@gmail.com>
2022-02-12 14:01:20 -0300

    feat: Requisito 5 - Criado o formulario para pesquisar artistas

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,12 +2,52 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      search: '',
+    };
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
   render() {
+    const { search } = this.state;
+    const minNameSearch = 2;
+    let buttonSearchValidation = true;
+
+    if (search.length >= minNameSearch) {
+      buttonSearchValidation = false;
+    }
+
     return (
-      <div data-testid="page-search">
+      <section data-testid="page-search">
         <Header />
-        <p>Search page</p>
-      </div>
+        <form>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            placeholder="Nome do Artista"
+            name="search"
+            value={ search }
+            onChange={ this.handleChange }
+          />
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ buttonSearchValidation }
+          >
+            Pesquisar
+          </button>
+        </form>
+      </section>
     );
   }
 }
